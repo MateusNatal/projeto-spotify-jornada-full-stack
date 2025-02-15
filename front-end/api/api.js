6005c95487d9e0e0c05c641d4bce5edbb4bbbbdc
Mateus Natal <mateusdaniel99@gmail.com>
2025-02-15 16:46:45 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,9 +3,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const { NODE_ENV } = process.env;
+//const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 
+const URL = "https://deploy-jornada-full-stack-ow52.onrender.com/api";
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
